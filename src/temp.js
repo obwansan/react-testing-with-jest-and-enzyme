b3bcd3bc5952854f5e335b444220fff9b8f081a4
Kevin O'Brien <kob123@hotmail.co.uk>
2019-08-25 12:13:51 +0100

    data-test attribute tests

--- src/temp.js
+++ src/temp.js
@@ -8,20 +8,42 @@ import App from './App';
 // Tells enzyme we're using React 16 and that's what it needs to use to create the virtual DOM.
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props sepcific to this setup.
+ * @param {any} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props={}, state=null) => {
+  // spread operator takes props object and turns it into individual props
+  return shallow(<App {...props} />)
+}
+
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute or search.
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test=${val}]`);
+}
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const appComponent = wrapper.find("[data-test='component-app']");
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const button = wrapper.find("[data-test='increment-button']");
   expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const counterDisplay = wrapper.find("[data-test='counter-display']");
   expect(counterDisplay.length).toBe(1);
 });
